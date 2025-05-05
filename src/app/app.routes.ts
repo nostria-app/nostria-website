43403de4bfa07ec7dfb5242122d4a0a33a5b39af
SondreB <sondre@outlook.com>
2025-05-05 15:46:09 +0200

    feat: add Privacy and Terms components with routing and update footer links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { PremiumComponent } from './pages/premium/premium.component';
 import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 import { AboutComponent } from './pages/about/about.component';
 import { FundingComponent } from './pages/funding/funding.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { TermsComponent } from './pages/terms/terms.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +21,8 @@ export const routes: Routes = [
   { path: 'features', component: FeaturesComponent },
   { path: 'premium', component: PremiumComponent },
   { path: 'funding', component: FundingComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'privacy', component: PrivacyComponent },
   { path: 'testimonials', component: TestimonialsComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' } // Catch any unfound routes and redirect to home
