4b69009f9b124bcddcb560a7566b140e5bc7b238
SondreB <sondre@outlook.com>
2025-12-29 14:26:57 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/pages/press/nostria-2-release/nostria-2-release.component.ts
+++ src/app/pages/press/nostria-2-release/nostria-2-release.component.ts
@@ -1,9 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-nostria-2-release',
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './nostria-2-release.component.html',
   styleUrl: './nostria-2-release.component.scss'
