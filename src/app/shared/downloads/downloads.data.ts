67fe6f5696e5f4255fa400f947bd12b605443ea4
SondreB <sondre@outlook.com>
2026-04-28 20:31:22 +0200

    Update currentDesktopRelease to version 3.1.32

--- src/app/shared/downloads/downloads.data.ts
+++ src/app/shared/downloads/downloads.data.ts
@@ -18,7 +18,7 @@ export interface DownloadPlatform {
   options: readonly DownloadOption[];
 }
 
-export const currentDesktopRelease = '3.1.18';
+export const currentDesktopRelease = '3.1.32';
 
 function createReleaseAssetUrl(fileName: string): string {
   return `https://github.com/nostria-app/nostria/releases/download/v${currentDesktopRelease}/${fileName}`;
